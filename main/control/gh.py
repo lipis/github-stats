f0d70c6017766e3281857bafcf7947a5662517f2
Panayiotis Lipiridis <lipiridis@gmail.com>
2015-12-20 18:35:34 +0100

    More tests

--- main/control/gh.py
+++ main/control/gh.py
@@ -4,20 +4,38 @@ import github
 import flask
 
 import config
+import model
 
 from main import app
 
 
-@app.route('/gh/<username>')
+@app.route('/<username>')
 def gh(username):
-  g = github.Github()
-  try:
-    account = g.get_user(username)
-  except github.GithubException as error:
-    return flask.abort(error.status)
+  username = username.lower()
+  account_db = model.Account.get_by_id(username)
 
-  repos = []
-  for project in account.get_repos():
-    repos.append(project.name)
+  if not account_db:
+    g = github.Github()
+    try:
+      account = g.get_user(username)
+    except github.GithubException as error:
+      return flask.abort(error.status)
 
-  return '%r' % ' '.join(repos)
+    account_db = model.Account.get_or_insert(
+        account.login,
+        username=account.login,
+        name=account.name,
+        avatar_url=account.avatar_url.split('?')[0],
+        organization=account.type == 'Organization',
+      )
+
+    repos = []
+    for project in account.get_repos():
+      repos.append(project.name)
+
+  return flask.render_template(
+      'account/account_view.html',
+      html_class='account-view',
+      title=account_db.username,
+      account_db=account_db,
+    )
